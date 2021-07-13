62c186534d9f8b388ba6aa5dbdde4bb2c230c3dd
BromTeque <aleksander@lingstad.no>
2021-07-13 18:05:30 +0200

    get archive path with checks

--- script.sh
+++ script.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "Hello, World!"
+declare -a entries_array
+
+# echo Please Enter path to YT archive folder:
+# read archivePath
+# echo path: $archivePath
+
+# Temporary Variable for Debugging
+archivePath=/home/bromteque/tstest
+
+# Checks if Path is Valid
+[ ! -d $archivePath ] && echo "Directory $archivePath DOES NOT exists. Enter valid path"
+
+for entry in "$archivePath"/*
+do
+    entries_array+=( $entry )
+done
+
+echo ${entries_array[*]}
+
+exit 0
